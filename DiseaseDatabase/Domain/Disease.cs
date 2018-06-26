0f2738fe2dc8a7c0c48fa4d3381552a6e6ef1649
Alo Avi <aavi@itcollege.ee>
2018-06-26 16:24:53 +0300

    Add Symtoms and Dieseases to Domain

---
+++ DiseaseDatabase/Domain/Disease.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Disease
+    {
+        public int DiseaseId { get; set; }
+        [Required]
+        [MaxLength(120)]
+        public string DiseaseName { get; set; }
+    }
+}
---
+++ DiseaseDatabase/Domain/Symptom.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Symptom
+    {
+        public int SymptomId { get; set; }
+        [Required]
+        [MaxLength(120)]
+        public string SymptomName { get; set; }
+    }
+}
